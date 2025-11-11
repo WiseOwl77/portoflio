b128990b3e02bc0db2af43e8139bc54a674c378e
Faten Fared <asffg691@gmail.com>
2025-11-11 00:42:38 -0800

    section contact-us

--- src/components/4.contact/Contact.jsx
+++ src/components/4.contact/Contact.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
-  return (
-    <div>CONTACT US</div>
-  )
-}
+	return (
+		<section className="contact-us">
+			<h1 className="title">
+				<span className="icon-envelope"></span>
+				Contact Us
+			</h1>
+			<p className="sub-title">
+				Contact us for more information and Get notified when I puplish
+				somting new.
+			</p>
+			<div className="flex">
+				<form className="">
+					<div className="flex">
+						<label htmlFor="email">Email Adress</label>
+						<input required type="email" name="" id="email" />
+					</div>
 
-export default Contact
+					<div className="flex" style={{ marginTop: "24px" }}>
+						<label htmlFor="message">Your message</label>
+						<textarea required name="" id="message"></textarea>
+					</div>
+					<button className="submit">Submit</button>
+				</form>
+				<div className="border animation">animation</div>
+			</div>
+		</section>
+	);
+};
+
+export default Contact;
