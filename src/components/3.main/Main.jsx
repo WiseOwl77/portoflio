8d687ab704e4290efc4ea70b6208c9d209b08ee1
Faten Fared <asffg691@gmail.com>
2025-11-10 21:53:30 -0800

    create items

--- src/components/3.main/Main.jsx
+++ src/components/3.main/Main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import './Main.css'
+import "./Main.css";
 
 function Main() {
-  return (
-    <div>Main</div>
-  )
+	return (
+		<main className="flex">
+			<section className="flex left-section">
+				<button className="active">All Projects</button>
+				<button>Html & Css</button>
+				<button>JavaScript</button>
+				<button>React</button>
+				<button>Node & Express</button>
+			</section>
+
+			<section className=" right-section">
+				{["nn", "ll", "kk"].map((item) => {
+					return (
+            	<article key={item} className="card">
+					<img width={266} src="" alt="" />
+					<div style={{ width: "266px" }} className="box">
+						<h1 className="title"></h1>
+						<p className="sub-title"></p>
+
+						<div className="flex icons">
+							<div style={{ gap: "11px" }} className="flex">
+								<div className="icon-link"></div>
+								<div className="icon-github"></div>
+							</div>
+							<a className="link flex" href="">
+								more
+								<span className="icon-arrow-right"></span>
+							</a>
+						</div>
+					</div>
+				</article>
+					);
+				})}
+			</section>
+		</main>
+	);
 }
 
-export default Main
+export default Main;
