11f3dedc61d32178b5d1aa520ebf1f3bdcae4894
Faten Fared <asffg691@gmail.com>
2025-11-13 16:36:58 -0800

    Footer

--- src/components/5.footer/Footer.jsx
+++ src/components/5.footer/Footer.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <div>FOOTER</div>
-  )
-}
+	return (
+		<footer className="flex">
+			<ul className="flex">
+				<li>
+					<a href="">About</a>
+				</li>
+				<li>
+					<a href="">Projects</a>
+				</li>
+				<li>
+					<a href="">Skills</a>
+				</li>
+				<li>
+					<a href="">contact</a>
+				</li>
+			</ul>
+			<p>2025 spencer sharp. All rights reserved.</p>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
